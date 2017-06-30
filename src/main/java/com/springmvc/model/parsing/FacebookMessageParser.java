c0ca355b8af71c3a18a2ab27cb5c617cd6e8d2e4
Nicholas Labarre <bitbucket@ndlr.xyz>
2017-06-30 18:08:09 -0400

    added basic facebook message parsing

---
+++ src/main/java/com/springmvc/model/parsing/FacebookMessageParser.java
@@ -0,0 +1,79 @@
+package com.springmvc.model.parsing;
+
+import com.sun.javaws.exceptions.InvalidArgumentException;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class FacebookMessageParser {
+    private static Pattern appNamePattern = Pattern.compile("^([a-zA-Z]+){1}");
+    private static Pattern argumentsPattern =
+            Pattern.compile("-([a-zA-Z-]+){1}\\s([^\"\\s]+|\"[^\"]+\"){1}");
+    private String message;
+
+    public FacebookMessageParser(String message) {
+        this.message = message;
+    }
+
+    public String getAppName() throws InvalidArgumentException {
+        Matcher matcher = appNamePattern.matcher(message);
+        String appName = parseAppname(matcher);
+
+        if (appName == null || appName.isEmpty())
+            throw new InvalidArgumentException(new String[]{message});
+
+        return appName;
+    }
+
+    private String parseAppname(Matcher matcher) {
+        String appName = null;
+
+        while (matcher.find())
+            appName = matcher.group(1);
+
+        return appName;
+    }
+
+    public Map<String, String> getArguments() throws InvalidArgumentException {
+        Matcher matcher = argumentsPattern.matcher(message);
+        Map<String, String> arguments = parseArguments(matcher);
+
+        if (arguments.isEmpty())
+            throw new InvalidArgumentException(new String[]{message});
+
+        return arguments;
+    }
+
+    private Map<String, String> parseArguments(Matcher matcher)
+            throws StringIndexOutOfBoundsException {
+        HashMap<String, String> arguments = new HashMap<>();
+
+        String argument, value = "";
+        while (matcher.find()) {
+            argument = matcher.group(1);
+            value = deserializeArgumentValue(matcher.group(2));
+
+            arguments.put(argument, value);
+        }
+
+        return arguments;
+    }
+
+    private String deserializeArgumentValue(String argumentValue)
+            throws StringIndexOutOfBoundsException {
+        if (argumentValue.startsWith("\"") && argumentValue.endsWith("\""))
+            argumentValue = argumentValue.substring(1, argumentValue.length() - 1);
+
+        return argumentValue;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}

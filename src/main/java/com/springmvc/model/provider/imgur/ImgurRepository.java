d2a92820569f7c9acd3500a0afc92b3fd75b139b
Nicholas Labarre <bitbucket@ndlr.xyz>
2017-07-18 20:53:19 -0400

    add ImgurRepository

---
+++ src/main/java/com/springmvc/model/provider/imgur/ImgurRepository.java
@@ -0,0 +1,8 @@
+package com.springmvc.model.provider.imgur;
+
+public class ImgurRepository {
+
+    public UploadResponse upload(UploadPayload payload) {
+        throw new RuntimeException("unsupported");
+    }
+}

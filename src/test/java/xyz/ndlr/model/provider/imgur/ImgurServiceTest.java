492387d2d77f5c0f9cddaa5d4e83f4984d2e348e
Nicholas Labarre <bitbucket@ndlr.xyz>
2017-08-26 14:38:09 -0400

    removed test tests

--- src/test/java/xyz/ndlr/model/provider/imgur/ImgurServiceTest.java
+++ src/test/java/xyz/ndlr/model/provider/imgur/ImgurServiceTest.java
@@ -15,8 +15,6 @@ import java.util.concurrent.atomic.AtomicInteger;
 public class ImgurServiceTest {
     private ImgurService imgurService;
 
-    private AtomicInteger atomicInteger = new AtomicInteger(0);
-
     @Before
     public void setUp() throws Exception {
         imgurService = new ImgurService(new ImgurRepository());
@@ -28,26 +26,5 @@ public class ImgurServiceTest {
 
     @Test
     public void testUpload() {
-        Attachment attachment = new Attachment();
-        attachment.setType(AttachmentType.IMAGE);
-        attachment.setPayload(new MultimediaPayload("https://www.google" +
-                ".com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png"));
-
-        Message message = new Message();
-        message.setAttachments(new ArrayList<>(Arrays.asList(attachment, attachment)));
-
-
-        try {
-            imgurService.upload(message);
-            System.out.println("We'll sleep now");
-            Thread.sleep(5000);
-            System.out.println("and now we're awake!");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    public void help() {
-        System.out.println("---\n" + atomicInteger.incrementAndGet() + "---\n");
     }
 }
